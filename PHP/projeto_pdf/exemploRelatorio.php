857e1029a34604eabead6ad1e251781deed9b0e9
Yohan Sie. <cautios.yohan@gmail.com>
2025-06-03 16:58:18 -0300

    Projeto de PDF completo - Básico
    Feito em aula de Desenvolvimento de Sistemas

--- PHP/projeto_pdf/exemploRelatorio.php
+++ PHP/projeto_pdf/exemploRelatorio.php
@@ -17,7 +17,7 @@ $pdf->addPage();
 // Funcao auxiliar para converter textos para ISO-8859-1 (evita problemas com acentos)
 function textoPDF($txt)
 {
-    return iconv('UTF-8', 'ISO-8859-1//TRANLIT', $txt);
+    return iconv('UTF-8', 'ISO-8859-1//TRANSLIT', $txt);
 }
 
 // Define a fonte Arial, Negrito (8), tamanho 18
@@ -36,3 +36,28 @@ $dados = [
     ['Item AG', 'Descricao 7', 'Categoria Z', 55.00],
 ];
 
+// Nome do Autor
+$pdf->SetFont('Arial', 'I', 12);
+$pdf->Cell(0, 40, 'Aluno: Yohan Siedschlag', 0, 1, 'C');
+
+// Cabeçalho da tabela
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(100, 20, textoPDF("Produto"), 1, 0, 'L');
+$pdf->Cell(180, 20, textoPDF("Detalhes"), 1, 0, 'L');
+$pdf->Cell(100, 20, textoPDF("Categorias"), 1, 0, 'L');
+$pdf->Cell(100, 20, textoPDF("Valor"), 1, 1, 'R');
+
+// Preenche a tabela com dados
+$pdf->SetFont('Arial', 'B', 10);
+foreach ($dados as $linha) {
+    $pdf->Cell(100, 20, textoPDF($linha[0]), 1, 0, 'L');
+    $pdf->Cell(180, 20, textoPDF($linha[1]), 1, 0, 'L');
+    $pdf->Cell(100, 20, textoPDF($linha[2]), 1, 0, 'L');
+    $pdf->Cell(100, 20, number_format($linha[3], 2, ',', '.'), 1, 1, "R");
+}
+
+// Finaliza PDF
+$pdf->Output("relatorio_produtos.pdf", "I");
+
+// I = inline - página web
+// D = Download
